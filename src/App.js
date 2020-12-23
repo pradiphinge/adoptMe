a4006128147ae374193a1cd63d83511f9935f8b7
unknown <hingepradeepster@gmail.com>
2020-12-23 11:51:45 +0530

    lazily load code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import SearchParams from "./components/SearchParams";
 import { Router } from "@reach/router";
-import Details from "./pages/Details";
+//import Details from "./pages/Details";
 //import Header from "./components/Header";
 import ThemeContext from "./ThemeContext";
 import NavBar from "./components/NavBar";
-
+const Details = lazy(() => import("./pages/Details"));
 function App() {
   const themeHook = useState("darkblue");
   return (
     <ThemeContext.Provider value={themeHook}>
       <NavBar />
       <div className="home-page">
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        <Suspense fallback={<h1>Loading route...</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
